d6398b2057933d5c5d094970553956adb137bb8f
Adrian <adrian_zinko@op.pl>
2022-02-13 11:47:24 +0100

    finish up lp

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@chakra-ui/react';
+
+const Footer = () => (
+  <Box
+    textAlign="center"
+    p="5"
+    color="gray.600"
+    borderTop="1px"
+    borderColor="gray.200"
+  >
+    &copy; 2022 Dreams Agency
+  </Box>
+);
+
+export default Footer;
--- components/layuot/Layout.tsx
+++ components/layuot/Layout.tsx
@@ -1,15 +1,21 @@
 import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
+import Navbar from '../navbar/Navbar';
+import Footer from '../footer/Footer';
 
 const Layout = ({ children }: { children: ChildNode }) => (
   <>
     <Head>
-      <title>Reit</title>
+      <title>Dreams Agency</title>
     </Head>
     <Box maxWidth="1280px" m="auto">
-      <header>Navbar</header>
+      <header>
+        <Navbar />
+      </header>
       <main>{children}</main>
-      <footer>Footer</footer>
+      <footer>
+        <Footer />
+      </footer>
     </Box>
   </>
 );
