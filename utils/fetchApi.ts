0abacbb4f095cbb47810c63ea8db026db4779068
Adrian <adrian_zinko@op.pl>
2022-02-18 20:05:46 +0100

    remove abort controller

--- utils/fetchApi.ts
+++ utils/fetchApi.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 export const baseUrl = 'https://bayut.p.rapidapi.com';
-export const controller = new AbortController();
-
 
 export const fetchApi = async (url: string) => {
   const { data } = await axios.get(url, {
@@ -10,7 +8,6 @@ export const fetchApi = async (url: string) => {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
       'x-rapidapi-key': process.env.NEXT_PUBLIC_BAYUT_API_KEY as string,
     },
-    signal: controller.signal
   });
 
   return data;
